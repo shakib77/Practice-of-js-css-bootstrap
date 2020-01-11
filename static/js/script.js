83ac1bea23b1bd52e28b0c2eb46a9a858d0c3680
shakib77 <shakibshaon77@gmail.com>
2020-01-12 00:07:23 +0600

    First parctice is completed!

---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+// Challenge 1: Your Age in Days
+
+function ageInDays() {
+    var birthYear = prompt ('What age are you born in?');
+    var ageInDayss = (2020 - birthYear) * 365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days old.')
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset(){
+    document.getElementById('ageInDays').remove();
+}
