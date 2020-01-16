93f878a69a0588f82f00c394a87a871ee8b4b4b9
shakib77 <shakibshaon77@gmail.com>
2020-01-16 19:42:14 +0600

    Cat/Image generator

--- static/js/script.js
+++ static/js/script.js
@@ -13,3 +13,12 @@ function ageInDays() {
 function reset(){
     document.getElementById('ageInDays').remove();
 }
+
+// Challenge 2: Cat Generator
+
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "https://cdn2.thecatapi.com/images/4do.gif";
+    div.appendChild(image);
+}
